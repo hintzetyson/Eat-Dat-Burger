07d0306058b3b9f9b02228bbe1b68e6d7fbefc97
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-06-16 15:36:27 -0600

    Added models for burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,31 @@
-const orm = require ("../config/orm")
+const orm = require ("../config/orm");
+
+const burger = {
+    selectAll: function(cb) {
+        orm.selectAll(function(data) {
+            cb(data)
+        });
+    },
+
+    insertOne: function(burgers, nameVal, cb) {
+        orm.insertOne(burgers, nameVal, function(results) {
+            cb(results)
+        })
+    },
+
+    updateOne: function(val, id, cb) {
+        let devoured = val.devoured;
+
+        orm.updateOne(devoured, id, function(res) {
+            cb(res)
+        });
+    },
+
+    deleteOne: function(id, cb) {
+        orm.deleteOne(id, (confirm) => {
+            cb(confirm)
+        });
+    }
+};
+
+module.exports = burger;
