cf70092b2ac87ccbee1b6ef8d9cf00a3aa972c91
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-06-22 15:22:43 -0600

    Added information to main, working on burger controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,4 +13,28 @@ router.get('/', function(req, res) {
     });
 });
 
-router.post()
+router.post('/burger', function(req,res) {
+    burger.insertOne(
+        [
+            'burger_name',
+        ],
+
+        [
+            req.body.burger_name,
+        ],
+
+        function(data) {
+            res.sendStatus(200).end();
+        }
+    )
+});
+
+router.put('/burgers/:id', function(req, res) {
+    let condition = req.params.id;
+
+    burger.updateOne(
+        {
+            devoured: true
+        }
+    )
+})
