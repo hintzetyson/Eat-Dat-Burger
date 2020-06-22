e0c085f8f8f3296ceb48094360ac4d9d2d5e29a5
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-06-22 15:32:06 -0600

    Added server.js information, set up server

---
+++ server.js
@@ -0,0 +1,25 @@
+//Express Dependencies
+var express = require("express");
+const expressHandle = require('express-handlebars');
+
+const PORT = process.env.PORT || 3000;
+
+let app = express();
+
+//Send content to app
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.json());
+
+app.engine('handlebars', expressHandle({ defaultLayout: 'main'}));
+
+//Grab the routes from the controller
+const routes = require('./controllers/burgers_controller');
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
