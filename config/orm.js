4eac6f25c788d4e45c8898e34741f9946fd699ee
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-06-26 08:39:10 -0600

    Added linting, start script

--- config/orm.js
+++ config/orm.js
@@ -1,35 +1,35 @@
 /* eslint-disable linebreak-style */
 const connection = require('./connection');
 
-var orm = {
-    selectAll: (cb) => {
-        var queryString = "SELECT * FROM burgers;";
-        connection.query(queryString, (err, result) => {
-            if (err) throw err;
-            cb(result);
-        });
-    },
-    insertOne: (inputs, cb) => {
-        var queryString = `INSERT INTO burgers (burger_name) VALUES (?);`;
-        connection.query(queryString, inputs, (err, result) => {
-            if (err) throw err;
-            cb(result);
-        });
-    },
-    updateOne: (inputs, cb) => {
-        var queryString = `UPDATE burgers SET devoured = true WHERE id = ?;`;
-        connection.query(queryString, inputs, (err, result) => {
-            if (err) throw err;
-            cb(result);
-        });
-    },
-    deleteOne: (inputs, cb) => {
-        var queryString = `DELETE FROM burgers WHERE id = ?`;
-        connection.query(queryString, inputs, (err, result) => {
-            if (err) throw err;
-            cb(result);
-        });
-    }
+const orm = {
+  selectAll: (cb) => {
+    const queryString = 'SELECT * FROM burgers;';
+    connection.query(queryString, (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  insertOne: (inputs, cb) => {
+    const queryString = 'INSERT INTO burgers (burger_name) VALUES (?);';
+    connection.query(queryString, inputs, (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  updateOne: (inputs, cb) => {
+    const queryString = 'UPDATE burgers SET devoured = true WHERE id = ?;';
+    connection.query(queryString, inputs, (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  deleteOne: (inputs, cb) => {
+    const queryString = 'DELETE FROM burgers WHERE id = ?';
+    connection.query(queryString, inputs, (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
+  },
 };
 
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-//Express Dependencies
-var express = require("express");
+/* eslint-disable linebreak-style */
+// Express Dependencies
+const express = require('express');
 const expressHandle = require('express-handlebars');
 
 const PORT = process.env.PORT || 3000;
 
-let app = express();
+const app = express();
 
-//Send content to app
+// Send content to app
 app.use(express.static('public'));
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.engine('handlebars', expressHandle({ defaultLayout: 'main'}));
-app.set("view engine", "handlebars");
+app.engine('handlebars', expressHandle({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-//Grab the routes from the controller
+// Grab the routes from the controller
 const routes = require('./controllers/burgers_controller');
 
 app.use(routes);
 
-app.listen(PORT, function() {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running on http://localhost:${PORT}`);
 });
